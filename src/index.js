fce6f07e9dbea1a3a7d581381ef0d1908070b8b6
tui-la-rua <nam11072k4c36@gmail.com>
2024-10-05 09:17:14 +0700

    scss

--- src/index.js
+++ src/index.js
@@ -4,15 +4,28 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-// Thiết lập handlebars như một view engine
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
 
+// Cấu hình file tĩnh
+app.use(express.static('./src/public'));
+
+// Thiết lập handlebars như một view engine
+// Thay đổi view engine
+app.engine('hbs', handlebars.engine(
+  {extname: '.hbs' }
+));
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views');
+
+// http logger
 app.use(morgan('dev'));
 
 // Render template index.handlebars
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Nodejs 2</h1>');
+  res.render('home')
+});
+
+app.get('/news', (req, res) => {
+  res.render('news')
 });
 
 app.listen(port, () => {
