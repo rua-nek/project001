78869fd588abf80b6acf69123b73173987f4f61d
tui-la-rua <nam11072k4c36@gmail.com>
2024-10-09 14:15:43 +0700

    post

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-
 // Cấu hình file tĩnh
 app.use(express.static('./src/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Thiết lập handlebars như một view engine
 // Thay đổi view engine
@@ -24,8 +25,13 @@ app.use(morgan('dev'));
 app.get('/home', (req, res) => {
   res.render('home')
 });
+app.post('/home', (req, res) => {
+  console.log(req.body);
+  res.render('home')
+});
 
 app.get('/news', (req, res) => {
+  console.log(req.query);
   res.render('news')
 });
 
